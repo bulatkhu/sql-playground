936485f1b369c701e4f03af5016d7ce80d862bfe
bulatkhu <hudainazarov.b@gmail.com>
2024-06-12 20:07:28 +0200

    added query for task 15, 16

--- queries/week9/queries.sql
+++ queries/week9/queries.sql
@@ -64,3 +64,59 @@ inner join album al on al.artist_id = ar.artist_id
 join track t on t.album_id = al.album_id
 group by ar.name
 order by avg(t.milliseconds);
+
+-- Frage 15:
+-- select * from artist ar
+-- join album a on a.artist_id = ar.artist_id;
+SELECT
+    count(*)
+FROM
+    artist ar
+        LEFT JOIN
+    album al ON ar.artist_id = al.artist_id
+WHERE
+    al.artist_id IS NULL;
+
+
+select al.album_id from album al
+join artist ar on al.artist_id = ar.artist_id
+group by al.album_id;
+
+select album.album_id from album;
+
+-- Frage 16:
+WITH ArtistAlbumCount AS (
+    SELECT
+        ar.artist_id,
+        ar.name,
+        COUNT(al.album_id) AS album_count
+    FROM
+        artist ar
+            JOIN
+        album al ON ar.artist_id = al.artist_id
+    GROUP BY
+        ar.artist_id,
+        ar.name
+    HAVING
+        COUNT(al.album_id) >= 5
+),
+     ArtistTrackLength AS (
+         SELECT
+             ar.name,
+             AVG(t.milliseconds) AS avg_track_length
+         FROM
+             ArtistAlbumCount ar
+                 JOIN
+             album al ON ar.artist_id = al.artist_id
+                 JOIN
+             track t ON al.album_id = t.album_id
+         GROUP BY
+             ar.name
+     )
+SELECT
+    name
+FROM
+    ArtistTrackLength
+ORDER BY
+    avg_track_length DESC
+LIMIT 1;
