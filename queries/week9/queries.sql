bf22f03ee5b96a99b2bf431bdf68b67df832b82c
bulatkhu <hudainazarov.b@gmail.com>
2024-06-12 17:22:47 +0200

    added queries for tasks 6 and 7

--- queries/week9/queries.sql
+++ queries/week9/queries.sql
@@ -4,4 +4,11 @@ select count(*) from "track"; -- 3503
 select * from "track" order by track.track_id desc;
 
 -- Frage 5;
-select count(*) from  "track" where milliseconds < 10000; -- 5
+select count(*) as quantity from  "track" where milliseconds < 10000; -- 5
+
+-- Frage 6:
+select * from "artist" where name ILIKE 'Santana%';
+select count(*) as Santanas from "artist" where name ILIKE 'Santana%';
+
+-- Frage 7;
+select count(*) from track where composer = 'Chris Cornell' or composer = 'Kurt Cobain'; -- 49
